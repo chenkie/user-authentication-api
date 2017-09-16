ec159beeee453f5e77358d4a232e5e284cc6f662
Ryan Chenkie <ryanchenkie@Ryans-MacBook-Pro.local>
2017-09-15 21:12:28 -0400

    add role

--- util/token.js
+++ util/token.js
@@ -14,16 +14,21 @@ function createToken(user) {
   }
 
   // Sign the JWT
-  return jwt.sign({
-    sub: user.id,
-    username: user.username,
-    email: user.email,
-    gravatar: createGravatarUrl(user.email),
-    scope
-  }, secret, { 
-    algorithm: 'HS256',
-    expiresIn: "1h" 
-  });
+  return jwt.sign(
+    {
+      sub: user.id,
+      username: user.username,
+      email: user.email,
+      role: 'admin',
+      gravatar: createGravatarUrl(user.email),
+      scope
+    },
+    secret,
+    {
+      algorithm: 'HS256',
+      expiresIn: '1h'
+    }
+  );
 }
 
 module.exports = createToken;
